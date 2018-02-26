f8d45c8c91962420b499bf5e2f88e5460569984f
« <emmajouffroypro@gmail.com>
2018-02-26 19:54:23 +0100

    header img + input borders

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,8 +1,26 @@
+
 <header>
-  <h1>SmartForms'</h1>
-  <ul>
-    <li>Mes campagnes</li>
-    <li>Nouvelle campagne</li>
-    <li>Déconnexion</li>
-  </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<a class="navbar-brand" href="#">SmartForms'</a>
+
+<div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+<li class="nav-item">
+  <a class="nav-link" href="#">Mes campagnes</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">Nouvelle campagne</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">Mes questionnaires</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">Déconnexion</a>
+</li>
+</ul>
+</div>
+</nav>
 </header>
--- index.php
+++ index.php
@@ -11,35 +11,40 @@
   </head>
   <body>
     <?php require("includes/connect.php") ?>
-<a href="images/logo.png"></a>
-<h1>Bienvenue sur SmartForm's</h1>
-<h3>Pour créer plus facilement vos formulaires</h3>
-<ul>
-  <li>Créez simplement vos formulaires</li>
-  <li>Visualisez les résultats de vos campagnes</li>
-  <li>Téléchargez ces résultats au format CSV</li>
-</ul>
-<h2>Inscription</h2><a href="pages/signin.php"> / Connexion</a>
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">@mail</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Mot de passe à nouveau</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="paper-toggle">
-    <label for="droits">Expérimentateur</label>
-  	<input type="checkbox" id="droits" name="droits" class="switch" />
-  	<label for="droits">Utilisateur</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Inscription</button>
-</form>
+    <div class="left_part">
+      <a href="images/logo.png"></a>
+      <h1>Bienvenue sur SmartForm's</h1>
+      <h3>Pour créer plus facilement vos formulaires</h3>
+      <ul>
+        <li>Créez simplement vos formulaires</li>
+        <li>Visualisez les résultats de vos campagnes</li>
+        <li>Téléchargez ces résultats au format CSV</li>
+      </ul>
+    </div>
+    <div class="right_part">
+
+      <h2>Inscription</h2><a href="pages/signin.php"> / Connexion</a>
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">@mail</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Mot de passe à nouveau</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="paper-toggle">
+          <label for="droits">Expérimentateur</label>
+        	<input type="checkbox" id="droits" name="droits" class="switch" />
+        	<label for="droits">Utilisateur</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Inscription</button>
+      </form>
+    </div>
 </body>
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="js/functions.js"></script>
